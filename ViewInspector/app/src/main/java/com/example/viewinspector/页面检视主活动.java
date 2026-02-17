50fe2610645ed708abee9238030cb07d78adfe5a
M17764017422 <31874069+M17764017422@users.noreply.github.com>
2026-02-17 22:56:50 +0800

    更新 页面检视主活动.java

--- ViewInspector/app/src/main/java/com/example/viewinspector/页面检视主活动.java
+++ ViewInspector/app/src/main/java/com/example/viewinspector/页面检视主活动.java
@@ -3,6 +3,7 @@ package com.example.viewinspector;
 import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.Configuration;
